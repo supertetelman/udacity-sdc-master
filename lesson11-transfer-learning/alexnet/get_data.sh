62c1056a12117a6bd8c2d9621c88ec7dc9309af6
Adam Tetelman <adam@techabt.com>
2017-03-21 17:11:02 -0700

    Add Transfer Learning lesson scripts to download data.

---
+++ lesson11-transfer-learning/alexnet/get_data.sh
@@ -0,0 +1,2 @@
+curl -o train.p https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580a829f_train/train.p
+curl -o bvlc-alexnet.npy https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d880c_bvlc-alexnet/bvlc-alexnet.npy
---
+++ lesson11-transfer-learning/transfer-learning/get_data.sh
@@ -0,0 +1,3 @@
+curl -o vgg-100.zip https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b432_vgg-100/vgg-100.zip 
+curl -o resenet-100.zip https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b634_resnet-100/resnet-100.zip
+curl -o inception-100.zip https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b498_inception-100/inception-100.zip
